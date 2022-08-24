6602ce0e2496d4cbcfc536f02f75f20d5bb4f9de
shavkatbegmatov <fstudioadm@gmail.com>
2022-08-24 23:25:28 +0500

    English Tests

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>English Tests</title>
     <link rel="stylesheet" type="text/css" href="/public/assets/bootstrap/css/bootstrap.css" />
     <link rel="stylesheet" type="text/css" href="/public/assets/css/style.css" />
 </head>
